82ef525a010179a72241707c0b13325b46797950
Eric M <emarvets@gmail.com>
2020-11-04 14:52:37 -0500

    Removed file

--- qBotJr/Types/ConfigT.fs
+++ qBotJr/Types/ConfigT.fs
@@ -1,22 +1,23 @@
 ﻿namespace qBotJr.T
 
-
-
 [<Struct>]
-type BotSettings = {
-    DiscordToken : string;
-    LogFileRoot : string;
-    GuildSettingsRoot : string
-    }
-[<Struct>]
-type GuildSettings = 
+type BotSettings =
     {
-    GuildID : uint64;
-    AdminRoles : uint64 list;
+    DiscordToken : string
+    LogFileRoot : string
+    GuildSettingsRoot : string
+
+    }
+
+[<Struct>]
+type GuildSettings =
+    {
+    GuildID : uint64
+    AdminRoles : uint64 list
     CaptainRoles : uint64 list
-    LobbiesCategory : uint64 option;
-    PlayersPerGame : int;
+    LobbiesCategory : uint64 option
+    PlayersPerGame : int
     AnnounceChannel : uint64 option
     }
-    static member defaultGuild id = 
+    static member defaultGuild id =
         {GuildSettings.GuildID = id ; AdminRoles = []; CaptainRoles = []; LobbiesCategory = None; PlayersPerGame = 9; AnnounceChannel = None}
